a5ba60dbd9b6c55f197c264f747b705a9082e881
Piyush Subedi <piy.subedi@gmail.com>
2021-09-07 19:55:59 -0400

    Number of unread messages is not displayed

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Box, Typography } from "@material-ui/core";
+import Grid from '@material-ui/core/Grid';
+import Badge from '@material-ui/core/Badge';
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
     justifyContent: "space-between",
     marginLeft: 20,
     flexGrow: 1,
@@ -43,16 +44,25 @@ const ChatContent = (props) => {
         <Typography className={classes.username}>
           {otherUser.username}
         </Typography>
-        { otherUser.isTyping && 
-          <Typography className={classes.previewText + ' ' + classes.typingText}>
-            {typingDisplayText}
-          </Typography>
-        }
-        { !otherUser.isTyping && 
-          <Typography className={numberOfUnreadMsgs > 0 ? classes.unreadText : classes.previewText}>
-            {latestMessage.text}
-          </Typography>
-        }
+        <Grid container spacing={3}>
+          <Grid item xs={8}>
+            { otherUser.isTyping && 
+              <Typography className={classes.previewText + ' ' + classes.typingText}>
+                {typingDisplayText}
+              </Typography>
+            }
+            { !otherUser.isTyping && 
+              <Typography className={numberOfUnreadMsgs > 0 ? classes.unreadText : classes.previewText}>
+                {latestMessage.text}
+              </Typography>
+            }
+          </Grid>
+          <Grid item xs={4}>
+            { !otherUser.isTyping && 
+              <Badge badgeContent={numberOfUnreadMsgs} color="primary"></Badge>
+            }
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
